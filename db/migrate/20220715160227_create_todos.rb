2023a7d556e81e56ebfe318449956aed4834b128
Ryan Delos Santos <ryansantos.delossantos121@gmail.com>
2022-07-18 12:59:45 -0500

    Add category name to table

--- db/migrate/20220715160227_create_todos.rb
+++ db/migrate/20220715160227_create_todos.rb
@@ -2,8 +2,9 @@ class CreateTodos < ActiveRecord::Migration[6.1]
   def change
     create_table :todos do |t|
       t.string :task
+      t.string :category_name
+      t.boolean :completed 
       t.integer :category_id
-      t.boolean :completed
     end
   end
 end
