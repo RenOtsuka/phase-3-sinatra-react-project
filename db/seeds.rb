9efabf6ebe4d11bd2e237d5cf00ba415fbc07ed9
Ryan Delos Santos <ryansantos.delossantos121@gmail.com>
2022-08-01 11:52:27 -0500

    Update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,19 @@
 puts "🌱 Seeding spices..."
 
 # Seed your database here
-Category.create(name: "Food")
-Category.create(name: "Tech")
-Category.create(name: "Misc")
+food = Category.create(name: "Food")
+tech = Category.create(name: "Tech")
+misc = Category.create(name: "Misc")
 
 
-Todos.create(task: "Buy Milk", category_name: "Food", completed: false, category_id: 1)
-Todos.create(task: "Buy Cheese", category_name: "Food", completed: true, category_id: 1)
+Todos.create(task: "Buy Milk", category_id: food.id)
+Todos.create(task: "Buy Cheese", category_id: food.id)
 
-Todos.create(task: "Upgrade Internet", category_name: "Tech", completed: true, category_id: 2)
-Todos.create(task: "Buy RAM", category_name:  "Tech", completed: false, category_id: 2)
+Todos.create(task: "Upgrade Internet", category_id: tech.id)
+Todos.create(task: "Buy RAM", category_id: tech.id)
 
-Todos.create(task: "Do Taxes", category_name: "Misc",  completed: false, category_id: 3)
-Todos.create(task: "Walk the Dog", category_name: "Misc", completed: false, category_id: 3)
+Todos.create(task: "Do Laundry", category_id: misc.id)
+Todos.create(task: "Walk the Dog", category_id: misc.id)
 
 
 puts "✅ Done seeding!"
